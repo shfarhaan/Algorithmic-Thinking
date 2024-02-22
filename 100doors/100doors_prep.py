082e2d1f7f60e12f412cefbbb4a1ad440b7d3f4e
farhaan-sh <shf.cse@gmail.com>
2024-02-22 22:53:00 +0600

    first commit

---
+++ 100doors/100doors_prep.py
@@ -0,0 +1,15 @@
+# doors = [False] * 101
+# print(doors) 
+
+# for i in range(1, 101):
+#     doors[i] = not doors[i]
+    
+# print(doors)
+
+for i in range(1, 6):
+    for j in range(1, 3):
+        print("i: ", i, "j: ", j)
+
+
+# for i in range(1, 101, 2):
+#     print(i)
