3ac63f9e9b38c4034e8943b4e4ae4b390fe9b724
farhaan-sh <shf.cse@gmail.com>
2024-02-23 23:28:53 +0600

    p2

---
+++ hands-on/str.py
@@ -0,0 +1,12 @@
+# # Python code​​​​​​‌​‌​‌‌​​​‌‌‌‌‌​‌‌‌‌​​‌‌‌‌ below
+# # Use print("messages...") to debug your solution.
+
+# show_expected_result = False
+# show_hints = False
+
+# Your code goes here
+def check_strings(first_string, second_string):
+    return first_string == second_string
+
+check_strings("appl", "apple")
+
